45834e76220bf3483f23b9377439bfa28913ecaa
gladOSportal <alexlegros202@gmail.com>
2022-05-09 17:10:48 +0200

    (message)

---
+++ td-1.js
@@ -0,0 +1,28 @@
+const text = document.getElementById("txt_to_add")
+const plist=document.querySelector('#mylist p')
+const parray=document.querySelector('#myarray p')
+const myarray=['tab1','tab2','tab3']
+
+
+function addtxt(){
+    if (text.value!=='') {
+        plist.innerText += text.value+'\n'
+        text.value=''
+    }
+
+    else{
+        alert ("write text")
+    }
+    textToAdd.focus();
+}
+
+
+function updatearray(){
+    for (let i=0; i<myarray.length; i++){
+        parray.innerText +=myarray[i]+'\n'
+    }
+}
+
+function pushtoarray(){
+
+}
